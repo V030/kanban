1fad61db074abac757ffee6d12304d4859f8ca4b
Jayvee Bernaldez <jayveebernaldez.basc@gmail.com>
2026-06-01 15:56:27 +0800

    added logging for db.js

--- server/config/db.js
+++ server/config/db.js
@@ -9,6 +9,13 @@ export const pool = new Pool ({
     connectionString: process.env.DATABASE_URL,
 });
 
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
+console.log("DATABASE_URL length:", process.env.DATABASE_URL?.length);
+console.log(
+  "DATABASE_URL start:",
+  process.env.DATABASE_URL?.substring(0, 50)
+);
+
 pool.on("connect", () => {
   console.log("Connected to PostgreSQL");
 });
